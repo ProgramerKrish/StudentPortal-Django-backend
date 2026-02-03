9f5252303375b85f3ee5b289ea97750804316b50
ProgramerKrish <krishnachaithanya1307@gmail.com>
2026-02-03 23:26:30 +0530

    explored-shellAPI-views-adminPage

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("hello world,at polls index")
 
+def detail(request,question_id):
+    return HttpResponse("you're looking at question %s. "%question_id)
+
+def results(request,question_id):
+    reponse="you're loking at the result of question %s."
+    return HttpResponse(reponse % question_id)
+def vote(request,quetion_id):
+    return HttpResponse("you're voting on question %s." % quetion_id)
